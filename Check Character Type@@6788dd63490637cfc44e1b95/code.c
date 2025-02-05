3c5577996c97c8bb40cac7012cb728297534693e
CHHAVI <chhaviarora0911@gmail.com>
2025-02-05 10:51:14 +0530

    Solution to Check Character Type

--- Check Character Type@@6788dd63490637cfc44e1b95/code.c
+++ Check Character Type@@6788dd63490637cfc44e1b95/code.c
@@ -6,7 +6,7 @@ int main() {
     if(isalpha(ch)){
         if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'||ch=='A'||ch=='E'||ch='I'||ch=='O'||ch='U'){
         printf("Vowel");
-    }else{
+    else{
         printf("Consonant");
     }
     }
