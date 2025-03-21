c7ca1b2ed5bff00d165efc13079020cd81e93084
CHHAVI <chhaviarora0911@gmail.com>
2025-03-21 10:38:50 +0530

    Solution to Check if a String is a Palindrome

--- Check if a String is a Palindrome@@676c039d80e1714600f67642/code.c
+++ Check if a String is a Palindrome@@676c039d80e1714600f67642/code.c
@@ -19,7 +19,7 @@ int isPalindrome(const char *str) {
 int main() {
     char str[100];
 
-    printf("Enter a string: ");
+  
     fgets(str, sizeof(str), stdin);
     
     // Remove trailing newline character if present
