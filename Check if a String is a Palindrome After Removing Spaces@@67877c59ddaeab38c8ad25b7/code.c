a2cd7c7c4d254927ac8e1b39c2c43723520bd946
CHHAVI <chhaviarora0911@gmail.com>
2025-04-08 12:02:36 +0530

    Solution to Check if a String is a Palindrome After Removing Spaces

---
+++ Check if a String is a Palindrome After Removing Spaces@@67877c59ddaeab38c8ad25b7/code.c
@@ -0,0 +1,28 @@
+// Your code here...
+// Your code here...
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+
+bool isBinaryString(char *str) {
+    for (int i = 0; str[i] != '\0'; i++) {
+        if (str[i] != '0' && str[i] != '1') {
+            return false; // If any character is not '0' or '1', return false
+        }
+    }
+    return true; // All characters are '0' or '1'
+}
+
+int main() {
+    char str[100];
+    printf("");
+    scanf("%s", str);
+
+    if (isBinaryString(str)) {
+        printf("Yes\n");
+    } else {
+        printf("No\n");
+    }
+
+    return 0;
+}
