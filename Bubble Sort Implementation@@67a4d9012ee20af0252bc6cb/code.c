a340aecbae4ae52d0592c2f5da3248de1329daee
CHHAVI <chhaviarora0911@gmail.com>
2025-04-08 10:54:27 +0530

    Solution to Bubble Sort Implementation

--- Bubble Sort Implementation@@67a4d9012ee20af0252bc6cb/code.c
+++ Bubble Sort Implementation@@67a4d9012ee20af0252bc6cb/code.c
@@ -1,25 +1,37 @@
 #include <stdio.h>
 
-int main() {
-    int rows, number = 1, i, j, space;
-    
-    // Taking input from the user
-    scanf("%d", &rows);
-    
-    // Loop through each row
-    for (i = 1; i <= rows; i++) {
-        // Printing spaces to form the pyramid
-        for (space = 1; space <= rows - i; space++) {
-            printf("  ");
+void bubbleSort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        // Last i elements are already sorted
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                // Swap the elements
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
         }
-        // Printing numbers
-        for (j = 1; j <= i; j++) {
-            printf("%d ", number);
-            number++;
-        }
-        // Moving to the next line
-        printf("\n");
     }
-    
+}
+
+void printArray(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+int main() {
+    int arr[] = {64, 34, 25, 12, 22, 11, 90};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    printf("Unsorted array: \n");
+    printArray(arr, n);
+
+    bubbleSort(arr, n);
+
+    printf("Sorted array: \n");
+    printArray(arr, n);
+
     return 0;
 }
