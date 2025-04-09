04a17936cafb9e45d86272005489923c4bc656f1
CHHAVI <chhaviarora0911@gmail.com>
2025-04-09 11:08:10 +0530

    Solution to Streaming Service Subscription Management

--- Streaming Service Subscription Management@@67877d61dc107486ad5c2752/code.c
+++ Streaming Service Subscription Management@@67877d61dc107486ad5c2752/code.c
@@ -33,7 +33,7 @@ int main() {
         }
     }
 
-    printf("Basic: %d Users, Revenue: %.2f;", basicCount, basicRevenue);
+    printf("Basic: %d Users, Revenue: %.2f; ", basicCount, basicRevenue);
     printf("Standard %d Users, Revenue: %.2f;", standardCount, standardRevenue);
     printf("Premium %d Users, Revenue: %.2f\n", premiumCount, premiumRevenue);
 
