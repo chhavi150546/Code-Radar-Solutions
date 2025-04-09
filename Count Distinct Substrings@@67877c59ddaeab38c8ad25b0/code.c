5497d63e0be8749d6ae77d3124e18dbcf75e7861
CHHAVI <chhaviarora0911@gmail.com>
2025-04-09 11:17:05 +0530

    Solution to Count Distinct Substrings

--- Count Distinct Substrings@@67877c59ddaeab38c8ad25b0/code.c
+++ Count Distinct Substrings@@67877c59ddaeab38c8ad25b0/code.c
@@ -1,50 +1,37 @@
 #include <stdio.h>
 #include <string.h>
-#include <stdbool.h>
 
-#define MAX 1000
-
-// Function to compare two strings
-bool isSame(char str1[], char str2[]) {
-    return strcmp(str1, str2) == 0;
-}
-
-// Function to check if a substring already exists
-bool isPresent(char substrings[][MAX], int count, char *str) {
+int isUnique(char substrs[][1000], int count, char *s) {
     for (int i = 0; i < count; i++) {
-        if (isSame(substrings[i], str)) {
-            return true;
-        }
+        if (strcmp(substrs[i], s) == 0)
+            return 0;
     }
-    return false;
+    return 1;
 }
 
 int main() {
-    char str[MAX];
- 
-    scanf("%s", str);
-
-    int len = strlen(str);
-    char substrings[MAX * (MAX + 1) / 2][MAX];  // store all substrings
+    char str[1000], substrs[10000][1000];
     int count = 0;
+    
+    // Read input
+    fgets(str, sizeof(str), stdin);
+    
+    int len = strlen(str);
+    if (str[len - 1] == '\n') str[len - 1] = '\0';
+    len = strlen(str);
 
     // Generate all substrings
     for (int i = 0; i < len; i++) {
-        for (int j = i; j < len; j++) {
-            char temp[MAX] = "";
-            int index = 0;
+        for (int j = 1; j <= len - i; j++) {
+            char temp[1000];
+            strncpy(temp, str + i, j);
+            temp[j] = '\0';
 
-            for (int k = i; k <= j; k++) {
-                temp[index++] = str[k];
-            }
-            temp[index] = '\0';
-
-            if (!isPresent(substrings, count, temp)) {
-                strcpy(substrings[count++], temp);
+            if (isUnique(substrs, count, temp)) {
+                strcpy(substrs[count++], temp);
             }
         }
     }
-
-    printf(" %d\n", count);
+    printf("%d\n", count);
     return 0;
 }
