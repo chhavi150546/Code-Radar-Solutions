d1b4d8175f81a4dfe5843129a5beef3edec947ca
CHHAVI <chhaviarora0911@gmail.com>
2025-04-09 11:38:48 +0530

    Solution to Binary Search on a Sorted Array

--- Binary Search on a Sorted Array@@67c9a8aa6e98bd154cc82f47/code.c
+++ Binary Search on a Sorted Array@@67c9a8aa6e98bd154cc82f47/code.c
@@ -1,6 +1,22 @@
 // Your code here...
 #include <stdio.h>
+int main() {
+    int arr[] = {2, 4, 6, 8, 10, 12, 14};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    int target;
 
+    printf("Enter target element: ");
+    scanf("%d", &target);
+
+    int index = binarySearch(arr, size, target);
+
+    if (index != -1)
+        printf("Element found at index: %d\n", index);
+    else
+        printf("Element not found.\n");
+
+    return 0;
+}
 // Binary Search Function
 int binarySearch(int arr[], int size, int target) {
     int low = 0, high = size - 1;
@@ -19,20 +35,4 @@ int binarySearch(int arr[], int size, int target) {
     return -1; // Target not found
 }
 
-int main() {
-    int arr[] = {2, 4, 6, 8, 10, 12, 14};
-    int size = sizeof(arr) / sizeof(arr[0]);
-    int target;
 
-    printf("Enter target element: ");
-    scanf("%d", &target);
-
-    int index = binarySearch(arr, size, target);
-
-    if (index != -1)
-        printf("Element found at index: %d\n", index);
-    else
-        printf("Element not found.\n");
-
-    return 0;
-}
