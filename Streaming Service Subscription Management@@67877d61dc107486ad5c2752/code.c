b6dce5557c24cb802ee8c720c321b3aa12a0be41
CHHAVI <chhaviarora0911@gmail.com>
2025-04-09 11:07:48 +0530

    Solution to Streaming Service Subscription Management

--- Streaming Service Subscription Management@@67877d61dc107486ad5c2752/code.c
+++ Streaming Service Subscription Management@@67877d61dc107486ad5c2752/code.c
@@ -10,7 +10,7 @@ struct Subscription {
 
 int main() {
     int n;
-    printf("Enter number of users: ");
+   
     scanf("%d", &n);
 
     struct Subscription users[n];
@@ -33,9 +33,9 @@ int main() {
         }
     }
 
-    printf("Basic - Users: %d, Revenue: %.2f\n", basicCount, basicRevenue);
-    printf("Standard - Users: %d, Revenue: %.2f\n", standardCount, standardRevenue);
-    printf("Premium - Users: %d, Revenue: %.2f\n", premiumCount, premiumRevenue);
+    printf("Basic: %d Users, Revenue: %.2f;", basicCount, basicRevenue);
+    printf("Standard %d Users, Revenue: %.2f;", standardCount, standardRevenue);
+    printf("Premium %d Users, Revenue: %.2f\n", premiumCount, premiumRevenue);
 
     return 0;
 }
