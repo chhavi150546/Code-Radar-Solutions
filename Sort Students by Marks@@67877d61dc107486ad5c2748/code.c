2182677668513dcf5cead0c8047b1e18242b1fb1
CHHAVI <chhaviarora0911@gmail.com>
2025-04-09 10:35:10 +0530

    Solution to Sort Students by Marks

---
+++ Sort Students by Marks@@67877d61dc107486ad5c2748/code.c
@@ -0,0 +1,41 @@
+// Your code here...
+#include <stdio.h>
+#include <string.h>
+
+struct Student {
+    int roll;
+    char name[50];
+    float marks;
+};
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    struct Student s[n], temp;
+
+    // Input student details
+    for (int i = 0; i < n; i++) {
+        scanf("%d %s %f", &s[i].roll, s[i].name, &s[i].marks);
+    }
+
+    // Sort using bubble sort (descending by marks)
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (s[j].marks < s[j + 1].marks) {
+                temp = s[j];
+                s[j] = s[j + 1];
+                s[j + 1] = temp;
+            }
+        }
+    }
+
+    // Display sorted student list
+    printf("Sorted list (Descending order of marks):\n");
+    for (int i = 0; i < n; i++) {
+        printf("Roll Number: %d, Name: %s, Marks: %.2f\n",
+               s[i].roll, s[i].name, s[i].marks);
+    }
+
+    return 0;
+}
